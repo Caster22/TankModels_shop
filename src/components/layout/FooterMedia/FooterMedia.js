a28415f45ac9991b3b5c6aef80890874e2e2d036
Michał Kopacz <kopaczmichal093@gmail.com>
2020-11-03 20:19:51 +0100

    Add elements to FooterMedia Component

--- src/components/layout/FooterMedia/FooterMedia.js
+++ src/components/layout/FooterMedia/FooterMedia.js
@@ -1,6 +1,12 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
 import styles from './FooterMedia.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
+import { faPinterestP } from '@fortawesome/free-brands-svg-icons';
 
 /* Components */
 
@@ -8,7 +14,40 @@ const Component = () => {
   return (
     <div className={ styles.root }>
       <div className='container'>
-        div
+        <div className='row align-items-center py-2'>
+          <div className={'col-lg-6 col-md-6 col-12 text-center ' + styles.copyright}>
+            <p>©Copyright 2020 LMA | All Rights Reserved</p>
+          </div>
+          <div className={'col-lg-6 col-md-6 col-12 text-center ' + styles.socialMedia}>
+            <ul>
+              <li>
+                <a href='/#'>
+                  <FontAwesomeIcon icon={faTwitter}>Twitter</FontAwesomeIcon>
+                </a>
+              </li>
+              <li>
+                <a href='/#'>
+                  <FontAwesomeIcon icon={faFacebookF}>Facebook</FontAwesomeIcon>
+                </a>
+              </li>
+              <li>
+                <a href='/#'>
+                  <FontAwesomeIcon icon={faYoutube}>YouTube</FontAwesomeIcon>
+                </a>
+              </li>
+              <li>
+                <a href='/#'>
+                  <FontAwesomeIcon icon={faGooglePlusG}>Google Plus</FontAwesomeIcon>
+                </a>
+              </li>
+              <li>
+                <a href='/#'>
+                  <FontAwesomeIcon icon={faPinterestP}>Pinterest</FontAwesomeIcon>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
