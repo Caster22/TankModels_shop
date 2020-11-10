5677a9786365b378cdb936b37b5ad85b2632ab28
Michał Kopacz <kopaczmichal093@gmail.com>
2020-11-10 20:45:06 +0100

    Add simple tests to ImageSlider

---
+++ src/components/Features/Slider/Slider.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import  ImageSlider  from './Slider';
+
+const mockImage = 'https://b7.pngbarn.com/png/225/18/world-of-tanks-germany-self-propelled-gun-tank-destroyer-tank-png-clip-art.png';
+const mockProps = [mockImage, mockImage, mockImage, mockImage];
+
+describe('PromptInfo component', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<ImageSlider images={mockProps} />);
+    expect(component).toBeTruthy();
+  });
+});
